d76f8209e6dbcc44431c61c71d026896fd87f291
keywordtarget <vegavlogg@gmail.com>
2018-05-23 18:12:21 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# go to root
+cd
+
+# creating xmrig directory
+mkdir xmrig
+
+#entering xmrig directory
+cd xmrig
+
+# downloading xmrig package
+wget https://github.com/keywordtarget/xmrig-2.6.2-xenial-amd64/raw/master/xmrig-2.6.2-xenial-amd64.tar.gz
+
+# extract package
+tar xfvz xmrig-2.6.2-xenial-amd64.tar.gz
+
+# see directory
+ls
+
+# entering xmrig-2.6.2 directory
+cd xmrig-2.6.2/
+
+# atcivate hugepages
+echo 128 > /proc/sys/vm/nr_hugepages
+
+# installing screen
+apt-get install screen
+
+# run screen
+screen
